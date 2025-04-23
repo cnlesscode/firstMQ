aba53e3ae6b222bf06e2004c5f5e098847bb615c
GraceCode <5213606@qq.com>
2025-04-23 11:29:09 +0800

    v1.0.8

--- server/start.go
+++ server/start.go
@@ -6,16 +6,17 @@ import (
 )
 
 func Start() {
-	// 是否开启服务
-	if configs.FirstMQConfig.Enable != "yes" {
-		return
-	}
 
 	// 启动 ServerFinder 服务
 	// 开启条件 : 服务器ip == ServerFinderConfig.Host
 	go func() {
 		serverFinder.Start(configs.ServerFinderConfig)
 	}()
+
+	// 是否开启服务
+	if configs.FirstMQConfig.Enable != "yes" {
+		return
+	}
 
 	// 开启 WS 服务
 	go func() {
